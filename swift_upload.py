cab5b83722d0602670ebec0244dcee6ac97777eb
AlexIsBadWithComputers <alex.tennant@cybera.ca>
2019-12-19 15:59:46 -0700

    added skeleton for swift upload

--- swift_upload.py
+++ swift_upload.py
@@ -2,6 +2,7 @@ from keystoneauth1 import session
 from keystoneauth1.identity import v3
 from swiftclient.client import Connection
 from login import swift_me, swift_pass, swift_auth
+from swiftclient.service import SwiftUploadObject
 
 auth = v3.Password(auth_url = swift_auth,
                    username = swift_me,
@@ -18,12 +19,13 @@ swift_conn = Connection(session=keystone_session)
 
 container = 'alberta_twitter_data'
 
-with open('alberta_tweets_nov_2019', 'r', encoding='utf-8') as local:
+with open('alberta_tweets_2019', 'rb') as local:
     # print(local.readlines())
+    print(local)
     swift_conn.put_object(
         container,
         'nov_2019_tweets_backup.txt',
-        contents=local.read().encode('utf-8'),
+        contents=local,
         content_type='text/plain',
         chunk_size = 1024*100
     )
