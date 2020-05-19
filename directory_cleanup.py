4eaded75b1ae145c3c6273a136a2b906efa036cb
AlexIsBadWithComputers <alex.tennant@cybera.ca>
2020-05-19 17:31:27 -0600

    downgrades

--- directory_cleanup.py
+++ directory_cleanup.py
@@ -2,16 +2,19 @@ import os, time
 from pathlib import Path
 
 def file_age(filepath):
-    return (time.time() - os.path.getmtime(filepath)) / 60 / 60
+    return float((time.time() - os.path.getmtime(filepath)) / 60 / 60)
 
 
 # default age is two months
 def cleanup(age = 60 * 24):
     pwd = str(os.getcwd())
+
     for file_path in Path(pwd).glob('*_start.txt'):
+        print(file_age(file_path))
         if file_age(file_path) > age:
             print("removing", file_path)
             os.remove(file_path)
 
 if __name__ == '__main__':  
     cleanup()
+    print()
