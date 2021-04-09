3934d7a7a7d30e4b7d199c1d4f91a21bdcc47a9f
AlexIsBadWithComputers <alex.tennant@cybera.ca>
2021-04-09 15:31:21 -0600

    nevermind

--- auto_start.sh
+++ auto_start.sh
@@ -1,12 +1,10 @@
-tmux attach 
+
 if pgrep -f "python streamer.py.py" &>/dev/null; then
     echo "it is already running"
-    tmux detach
     exit
 else
     echo "Sanity check back ups: "
     python3 backup_auto.py
     echo "re-starting script "
     python streamer.py & 
-    tmux detach
 fi
