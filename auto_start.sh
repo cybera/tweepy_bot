17eb1687de8cc0c9a403e03033e45698a5e79671
AlexIsBadWithComputers <alex.tennant@cybera.ca>
2021-04-09 15:29:08 -0600

    needed to be in tmux session

--- auto_start.sh
+++ auto_start.sh
@@ -1,9 +1,12 @@
+tmux attach 
 if pgrep -f "python streamer.py.py" &>/dev/null; then
     echo "it is already running"
+    tmux detach
     exit
 else
     echo "Sanity check back ups: "
     python3 backup_auto.py
     echo "re-starting script "
-    python streamer.py
+    python streamer.py & 
+    tmux detach
 fi
